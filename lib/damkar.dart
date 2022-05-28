357fb0178caf059e2e76a8a9e7b4aa47becf506c
dwiarypm <puputdwiari@gmail.com>
2022-05-28 17:01:54 +0700

    progress damkar belum finish

--- lib/damkar.dart
+++ lib/damkar.dart
@@ -14,6 +14,63 @@ class _DamkarState extends State<Damkar> {
         title: Text("Layanan Damkar"),
         backgroundColor: Colors.white,
       ),
+      backgroundColor: Colors.green[100],
+      body: Container(
+          padding: EdgeInsets.all(30.0),
+          child: GridView.count(
+            crossAxisCount: 2,
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 200,
+                    child: Card(
+                      margin: EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: Colors.green,
+                        child: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Icon(
+                                Icons.home,
+                                size: 70.0,
+                              ),
+                              Text("Home", style: new TextStyle(fontSize: 17.0))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 200,
+                    child: Card(
+                      margin: EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {},
+                        splashColor: Colors.green,
+                        child: Center(
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Icon(
+                                Icons.home,
+                                size: 70.0,
+                              ),
+                              Text("Home", style: new TextStyle(fontSize: 17.0))
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )),
     );
   }
 }
