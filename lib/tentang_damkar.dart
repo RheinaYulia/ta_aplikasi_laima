0b128992e055f3fdcde6f0016a1fc61b8600d19b
RiskaDifani <idriska48@gmail.com>
2022-06-18 18:57:53 +0700

    tentang damkar

--- lib/tentang_damkar.dart
+++ lib/tentang_damkar.dart
@@ -9,24 +9,38 @@ class _TentangDamkarState extends State<TentangDamkar> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: new AppBar(
+    return Scaffold(
+      appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.menu,
-            color: Colors.black,
-            ),
-        ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text(
-          "Pemadam Kebakaran",
-          style: TextStyle(
-            color: Colors.black
+          icon: Icon(Icons.arrow_left),
+          tooltip: "Kembali",
+          onPressed:null,
           ),
+          title: Text("Pemadaman Kebakaran"),
+          backgroundColor: Colors.green[700],
+      ),
+      body: Container(
+        color: Colors.green[200],
+        constraints: BoxConstraints.expand(),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))
+          ),
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.all(40),
+          child: Column(
+            children: [
+              Image.asset('assets/images/lokdamkar.jpg',width: 400,),
+              Text("Pemadam Kebakaran", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 30),),
+              Text("Pemadam kebakaran disingkat Damkar, Branwir (dari Bahasa Belanda Brandweer), atau PMK adalah orang atau pasukan yang bertugas memadamkan kebakaran, melakukan penyelamatan, dan menanggulangi bencana atau kejadian lainya.", textAlign: TextAlign.center,),
+
+            ],
+            
+          ),
+         
         ),
-      ),  
-    );
+      ),
+    ); 
   }
 }
