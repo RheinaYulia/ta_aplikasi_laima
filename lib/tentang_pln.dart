9292a6b9cf4d217d7899b942449cc34d6c878c3e
RiskaDifani <idriska48@gmail.com>
2022-06-24 14:12:58 +0700

    tentang PLN dan Polisi

---
+++ lib/tentang_pln.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class TentangPln extends StatefulWidget {
+  @override
+  State<TentangPln> createState() => _TentangPlnState();
+}
+
+class _TentangPlnState extends State<TentangPln> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_left),
+          tooltip: "Kembali",
+          onPressed:null,
+          ),
+          title: Text("PLN"),
+          backgroundColor: Colors.green[700],
+      ),
+      body: Container(
+        color: Colors.green[200],
+        constraints: BoxConstraints.expand(),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))
+          ),
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.all(40),
+          child: Column(
+            children: [
+              Image.asset('assets/images/PLNMalang.jpg',width: 400,),
+              Text("PLN (Perusahaan Listrik Negara)", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 30),),
+              Text("PT. PLN (Persero) merupakan suatu badan usaha milik negara (BUMN) dibidang kelistrikan yang melayani masyarakat diseluruh nusantara, bertekad untuk memberikan pelayanan jasa ketenagalistrikan yang terbaik dan memenuhi standar ketenagalistrikan yang dapat diterima di dunia internasional.", textAlign: TextAlign.center,),
+
+            ],
+            
+          ),
+         
+        ),
+      ),
+    ); 
+  }
+}
--- lib/tentang_polisi.dart
+++ lib/tentang_polisi.dart
@@ -9,24 +9,38 @@ class _TentangPolisiState extends State<TentangPolisi> {
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-      appBar: new AppBar(
+    return Scaffold(
+      appBar: AppBar(
         leading: IconButton(
-          onPressed: () {},
-          icon: Icon(
-            Icons.menu,
-            color: Colors.black,
-            ),
-        ),
-        backgroundColor: Colors.white,
-        centerTitle: true,
-        title: Text(
-          "Polisi Kota Malang",
-          style: TextStyle(
-            color: Colors.black
+          icon: Icon(Icons.arrow_left),
+          tooltip: "Kembali",
+          onPressed:null,
           ),
+          title: Text("Polisi"),
+          backgroundColor: Colors.green[700],
+      ),
+      body: Container(
+        color: Colors.green[200],
+        constraints: BoxConstraints.expand(),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10.0))
+          ),
+          padding: EdgeInsets.all(20),
+          margin: EdgeInsets.all(40),
+          child: Column(
+            children: [
+              Image.asset('assets/images/lokpolice1.jpg',width: 400,),
+              Text("Polisi/Kepolisian", style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold,fontSize: 30),),
+              Text("Polisi adalah suatu pranata umum sipil yang menjaga ketertiban dan keamanan di seluruh wilayah negara. Kepolisian adalah salah satu lembaga penting yang memainkan tugas utama sebagai penjaga keamanan dan ketertiban dan sehingga lembaga kepolisian ada di seluruh negara berdaulat.", textAlign: TextAlign.center,),
+
+            ],
+            
+          ),
+         
         ),
-      ),  
-    );
+      ),
+    ); 
   }
 }
