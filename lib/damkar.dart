08d04ad91cfedf701b4912b10fabcbd40ae4e8cd
dwiarypm <puputdwiari@gmail.com>
2022-06-26 16:07:24 +0700

    Percobaan Dwiary

--- lib/damkar.dart
+++ lib/damkar.dart
@@ -1,4 +1,3 @@
-import 'dart:html';
 import 'package:flutter/material.dart';
 
 class Damkar extends StatefulWidget {
@@ -15,90 +14,89 @@ class _DamkarState extends State<Damkar> {
         backgroundColor: Colors.green[700],
       ),
       backgroundColor: Colors.green[200],
-      body: Container(
-          padding: EdgeInsets.all(30.0),
-          child: GridView.count(
-            crossAxisCount: 2,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    height: 200,
-                    child: Card(
-                      margin: EdgeInsets.all(8.0),
-                      child: InkWell(
-                        onTap: () {},
-                        splashColor: Colors.green,
-                        child: Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Icon(
-                                Icons.call_outlined,
-                                color: Colors.red,
-                                size: 70.0,
-                              ),
-                              Text("Call Now",
-                                  style: new TextStyle(fontSize: 17.0))
-                            ],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Container(
+                width: 350,
+                height: 200,
+                child: Card(
+                  margin: EdgeInsets.all(8.0),
+                  child: InkWell(
+                    onTap: () {},
+                    splashColor: Colors.green,
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: <Widget>[
+                          Icon(
+                            Icons.call_outlined,
+                            color: Colors.red,
+                            size: 70.0,
                           ),
-                        ),
+                          Text("Call Now", style: new TextStyle(fontSize: 17.0))
+                        ],
                       ),
                     ),
                   ),
-                  Container(
-                    height: 200,
-                    child: Card(
-                      margin: EdgeInsets.all(8.0),
-                      child: InkWell(
-                        onTap: () {},
-                        splashColor: Colors.green,
-                        child: Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Icon(
-                                Icons.map_outlined,
-                                color: Colors.blue[800],
-                                size: 70.0,
-                              ),
-                              Text("Lokasi Kantor",
-                                  style: new TextStyle(fontSize: 17.0))
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    height: 200,
-                    child: Card(
-                      margin: EdgeInsets.all(8.0),
-                      child: InkWell(
-                        onTap: () {},
-                        splashColor: Colors.green,
-                        child: Center(
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              Icon(
-                                Icons.info_outline,
-                                color: Colors.yellow[700],
-                                size: 70.0,
-                              ),
-                              Text("Tentang Damkar",
-                                  style: new TextStyle(fontSize: 17.0))
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
-            ],
-          )),
+            ),
+            Container(
+              width: 350,
+              height: 200,
+              child: Card(
+                margin: EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.green,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(
+                          Icons.map_outlined,
+                          color: Colors.blue[800],
+                          size: 70.0,
+                        ),
+                        Text("Lokasi Kantor",
+                            style: new TextStyle(fontSize: 17.0))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: 350,
+              height: 200,
+              child: Card(
+                margin: EdgeInsets.all(8.0),
+                child: InkWell(
+                  onTap: () {},
+                  splashColor: Colors.green,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        Icon(
+                          Icons.info_outline,
+                          color: Colors.yellow[700],
+                          size: 70.0,
+                        ),
+                        Text("Tentang Damkar",
+                            style: new TextStyle(fontSize: 17.0))
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
